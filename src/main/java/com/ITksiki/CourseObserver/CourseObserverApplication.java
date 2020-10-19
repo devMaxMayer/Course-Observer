a6da5aafe563d2a9104117ac8fc2e892d79927f6
devMaxMayer <dev.maxmayer@gmail.com>
2020-10-19 07:32:43 +0400

    Initial commit

---
+++ src/main/java/com/ITksiki/CourseObserver/CourseObserverApplication.java
@@ -0,0 +1,13 @@
+package com.ITksiki.CourseObserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseObserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ITksiki/CourseObserver/CourseObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITksiki.CourseObserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
